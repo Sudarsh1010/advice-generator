ce478922733c30877af48c745b306be63e54b755
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-03-23 10:47:17 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import pauseMobile from "./images/pattern-divider-mobile.svg"
+import pauseDesktop from "./images/pattern-divider-desktop.svg"
+import dice from "./images/icon-dice.svg"
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  const fetchAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json()
+
+    console.log(data)
+    setQuotes(data.slip)
+  }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Advice #{quotes.id}</h1>
+      <p>"{quotes.advice}"</p>
+
+      <picture>
+        <source
+          media="(min-width: 768px)"
+          srcSet={pauseDesktop}
+        />
+
+        <img
+          src={pauseMobile}
+          alt=""
+        />
+      </picture>
+
+      <div>
+        <button
+          onClick={fetchAdvice}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src={dice}
+            alt=""
+          />
+        </button>
+      </div>
     </div>
   );
 }
